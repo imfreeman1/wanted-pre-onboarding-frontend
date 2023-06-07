cee006c16562111617bbd9bd8f8ffb6752d5b05c
imfreeman1 <dkfrhkt@gmail.com>
2023-06-07 16:47:08 +0900

    Button Component style 생성

--- src/Component/Button/index.jsx
+++ src/Component/Button/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { StyledButton } from './style'
 
 const Button = ({onClick, children}) => {
   return (
-    <button onClick={onClick}>
+    <StyledButton onClick={onClick}>
         {children}
-    </button>
+    </StyledButton>
   )
 }
 
---
+++ src/Component/Button/style.jsx
@@ -0,0 +1,6 @@
+import {styled} from 'styled-components';
+
+export const StyledButton = styled.button`
+  width: 5em;
+  height: 2em;
+`;
