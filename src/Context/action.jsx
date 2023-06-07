fe4e28eaaf62a0f9f04aed3fdc36f05d8042f1af
imfreeman1 <dkfrhkt@gmail.com>
2023-06-07 20:15:49 +0900

    action 추가

--- src/Context/action.jsx
+++ src/Context/action.jsx
@@ -2,4 +2,11 @@ export const TODO_ACTION = {
   ADD: "add",
   REMOVE: "remove",
   MODIFY: "modify",
+  CHECK: "check",
+};
+
+export const SIGN_ACTION = {
+  EMAIL: "email",
+  PASSWORD: "password",
+  SUBMIT: "submit",
 };
