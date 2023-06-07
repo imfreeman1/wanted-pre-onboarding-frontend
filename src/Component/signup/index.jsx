cb1a01645406e52e1479b525a61104138e76a5eb
imfreeman1 <dkfrhkt@gmail.com>
2023-06-07 20:14:48 +0900

    SignUp Component 구현
     - 유효성 검사 기능 수정 필요.

--- src/Component/signup/index.jsx
+++ src/Component/signup/index.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React, { useCallback, useEffect, useReducer } from "react";
+import Input from "../Input";
+import Button from "../Button";
+import { signReducer } from "../../Context/signReducer";
+import { SignDispatch, SignTask } from "../../Context/signContext";
+import { SIGN_ACTION } from "../../Context/action";
+
+const initState = {
+  email: "",
+  password: "",
+  isSubmitting: false,
+};
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [signTask, dispatch] = useReducer(signReducer, initState);
 
-export default Signup
+  const handleEmail = (e) => {
+    dispatch({
+      type: SIGN_ACTION.EMAIL,
+      payload: { ...signTask, email: e.target.value },
+    });
+  };
+
+  const handlePassword = (e) => {
+    dispatch({
+      type: SIGN_ACTION.PASSWORD,
+      payload: { ...signTask, password: e.target.value },
+    });
+  };
+
+  const handleIsSubmit = useCallback(() => {
+    if (signTask.email.includes("@") && signTask.password.length >= 8) {
+      console.log("실행됨");
+      dispatch({
+        type: SIGN_ACTION.SUBMIT,
+        payload: { ...signTask, isSubmitting: true },
+      });
+    }
+  }, [signTask]);
+  handleIsSubmit();
+  return (
+    <SignTask.Provider value={signTask}>
+      <SignDispatch.Provider value={dispatch}>
+        <div>
+          <h1>회원가입</h1>
+          <form action="#">
+            <Input
+              placeholder={"email"}
+              value={signTask.email}
+              onChange={(e) => handleEmail(e)}
+              type="text"
+            />
+            <Input
+              placeholder={"password"}
+              value={signTask.password}
+              onChange={(e) => handlePassword(e)}
+              type="password"
+            />
+            <Button children={"제출"} disabled={!signTask.isSubmitting} />
+          </form>
+        </div>
+      </SignDispatch.Provider>
+    </SignTask.Provider>
+  );
+};
+
+export default Signup;
