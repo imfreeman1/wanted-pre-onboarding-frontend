e726d16d60b5e1e6146b48adb297172efe7ee9b8
imfreeman1 <dkfrhkt@gmail.com>
2023-06-07 16:47:48 +0900

    Router 변경 및 lib install

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signin from "./Component/signin";
 import Signup from "./Component/signup";
 import Home from "./Home";
@@ -6,17 +6,16 @@ import TodoContainer from "./Component/Todo";
 
 function App() {
   return (
-  <div>
-      <Router>
+    <div>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path='/todo' element={<TodoContainer />}/>
+          <Route path="/todo" element={<TodoContainer />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </div>
-   
   );
 }
 
