68b783be06bc53ef76be6465342dc108054b52ac
imfreeman1 <dkfrhkt@gmail.com>
2023-06-08 18:56:37 +0900

    TodoItem 수정
     - 사용하지 않는 props 정리
     - li tag key 추가.

--- src/Component/TodoItem/index.jsx
+++ src/Component/TodoItem/index.jsx
@@ -4,7 +4,7 @@ import Input from "../Input";
 import { TodoDispatch } from "../../Context/todoContext";
 import { TODO_ACTION } from "../../Context/action";
 
-const TodoItem = ({ todo, key }) => {
+const TodoItem = ({ todo }) => {
   const dispatch = useContext(TodoDispatch);
   const [isEditing, setIsEditing] = useState(false);
   const handleEditing = useCallback(() => {
@@ -32,28 +32,31 @@ const TodoItem = ({ todo, key }) => {
     });
   };
   return (
-    <li>
+    <li key={todo.id}>
       <div>
-        <Input
-          type="checkbox"
-          checked={todo.isCompleted}
-          onChange={(e) => handleCheck(e, todo)}
-        />
-        {!isEditing ? (
-          <>
-            <span>{todo.content}</span>
-            <Button children={"수정"} onClick={handleEditing} />
-          </>
-        ) : (
-          <>
-            <Input
-              type="text"
-              value={todo.content}
-              onChange={(e) => handleModify(e, todo)}
-            />
-            <Button children={"완료"} onClick={handleEditing} />
-          </>
-        )}
+        <label>
+          <Input
+            type="checkbox"
+            checked={todo.isCompleted}
+            onChange={(e) => handleCheck(e, todo)}
+          />
+          {!isEditing ? (
+            <>
+              <span>{todo.content}</span>
+              <Button children={"수정"} onClick={handleEditing} />
+            </>
+          ) : (
+            <>
+              <Input
+                type="text"
+                value={todo.content}
+                onChange={(e) => handleModify(e, todo)}
+              />
+              <Button children={"완료"} onClick={handleEditing} />
+            </>
+          )}
+        </label>
+
         <Button children={"삭제"} onClick={() => handleRemove(todo.id)} />
       </div>
     </li>
