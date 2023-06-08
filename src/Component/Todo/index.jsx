c123190ef97e59469d66a4d90714ce223c1938bf
imfreeman1 <dkfrhkt@gmail.com>
2023-06-08 18:57:24 +0900

    TodoContainer 수정
     - 리다이렉트 추가
     - ADD 통신 추가

--- src/Component/Todo/index.jsx
+++ src/Component/Todo/index.jsx
@@ -1,10 +1,12 @@
-import React, { useCallback, useReducer, useState } from "react";
+import React, { useCallback, useEffect, useReducer, useState } from "react";
 import Input from "../Input";
 import Button from "../Button";
 import { TodoDispatch, TodoTask } from "../../Context/todoContext";
 import { todoReducer } from "../../Context/todoReducer";
 import { TODO_ACTION } from "../../Context/action";
 import TodoItem from "../TodoItem";
+import { useNavigate } from "react-router-dom";
+import axios from "../../Utils/axios";
 
 const initTodoList = [];
 
@@ -19,15 +21,38 @@ initState = [
 */
 
 const TodoContainer = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/signin");
+    }
+  });
+
   const [todo, setTodo] = useState("");
   const [todoList, dispatch] = useReducer(todoReducer, initTodoList);
   const onChange = useCallback((e) => {
     setTodo(e.target.value);
   }, []);
-  const handleSubmit = useCallback(() => {
-    dispatch({ type: TODO_ACTION.ADD, payload: todo });
-    setTodo("");
-  }, [dispatch, todo]);
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      const request = await axios.request({
+        method: "post",
+        url: "/todos",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          "Content-Type": "application/json",
+        },
+        data: { todo },
+      });
+      console.log(request);
+      dispatch({ type: TODO_ACTION.ADD, payload: todo });
+      setTodo("");
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
 
   return (
     <TodoTask.Provider value={todoList}>
@@ -38,9 +63,9 @@ const TodoContainer = () => {
               <TodoItem todo={s} key={s.id} />
             ))}
           </ul>
-          <form action="#">
+          <form>
             <Input placeholder="입력" value={todo} onChange={onChange} />
-            <Button children={"완료"} onClick={handleSubmit} />
+            <Button children={"완료"} onClick={(e) => handleSubmit(e)} />
           </form>
         </div>
       </TodoDispatch.Provider>
