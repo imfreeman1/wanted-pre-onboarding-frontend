2a917d505c91cea633d8472fb0a89a7c1e0e7520
imfreeman1 <dkfrhkt@gmail.com>
2023-06-12 19:17:12 +0900

    TodoContainer 변경
     - 통신 함수명 변경, 호이스팅을 위해 선언문으로 변경
     - input 관련 함수와 태그를 TodoForm으로 분할

--- src/Component/Todo/index.jsx
+++ src/Component/Todo/index.jsx
@@ -1,12 +1,11 @@
-import React, { useCallback, useEffect, useReducer, useState } from "react";
-import Input from "../Input";
-import Button from "../Button";
+import React, { useEffect, useReducer } from "react";
 import { TodoDispatch, TodoTask } from "../../Context/todoContext";
 import { todoReducer } from "../../Context/todoReducer";
 import { TODO_ACTION } from "../../Context/action";
 import TodoItem from "../TodoItem";
 import { useNavigate } from "react-router-dom";
 import axios from "../../Utils/axios";
+import TodoForm from "../TodoForm";
 
 const initTodoList = [];
 
@@ -21,52 +20,42 @@ initState = [
 */
 
 const TodoContainer = () => {
+  const [todoList, dispatch] = useReducer(todoReducer, initTodoList);
   const navigate = useNavigate();
-
   useEffect(() => {
     if (!localStorage.getItem("token")) {
       navigate("/signin");
     }
-  });
-
-  const [todo, setTodo] = useState("");
-  const [todoList, dispatch] = useReducer(todoReducer, initTodoList);
-  const onChange = useCallback((e) => {
-    setTodo(e.target.value);
-  }, []);
-  const handleSubmit = async (e) => {
-    try {
-      e.preventDefault();
-      const request = await axios.request({
-        method: "post",
-        url: "/todos",
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`,
-          "Content-Type": "application/json",
-        },
-        data: { todo },
-      });
-      console.log(request);
-      dispatch({ type: TODO_ACTION.ADD, payload: todo });
-      setTodo("");
-    } catch (error) {
-      throw new Error(error);
+    if (localStorage.getItem("token")) {
+      getTodo();
     }
-  };
+    async function getTodo() {
+      try {
+        const result = await axios.request({
+          method: "get",
+          url: "/todos",
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        });
+        if (result.status === 200)
+          dispatch({ type: TODO_ACTION.GET, payload: result.data });
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
+  }, []);
 
   return (
     <TodoTask.Provider value={todoList}>
       <TodoDispatch.Provider value={dispatch}>
-        <div>
-          <ul>
-            {todoList?.map((s) => (
-              <TodoItem todo={s} key={s.id} />
-            ))}
-          </ul>
-          <form>
-            <Input placeholder="입력" value={todo} onChange={onChange} />
-            <Button children={"완료"} onClick={(e) => handleSubmit(e)} />
-          </form>
+        <div className="grid grid-cols-12 grid-rows-6 w-screen h-screen">
+          <div className=" row-start-2 row-span-4 col-start-5 col-span-4 flex flex-col gap-4">
+            <ul className="flex flex-col grow border-black border-2 rounded-md bg-indigo-100 overflow-y-auto py-3 ov">
+              {todoList?.map((s) => (
+                <TodoItem todo={s} key={s.id} />
+              ))}
+            </ul>
+            <TodoForm />
+          </div>
         </div>
       </TodoDispatch.Provider>
     </TodoTask.Provider>
