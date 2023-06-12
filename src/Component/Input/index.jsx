cdd55cb000b09636bb6ad7ddc2d1c55c86974cd8
imfreeman1 <dkfrhkt@gmail.com>
2023-06-12 19:11:16 +0900

    Input component 변경
     - className, onBlur, testID props 추가

--- src/Component/Input/index.jsx
+++ src/Component/Input/index.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
-const Input = ({ placeholder, value, onChange, type, checked }) => {
+const Input = ({
+  className,
+  placeholder,
+  value,
+  onChange,
+  onBlur,
+  type,
+  checked,
+  testID,
+}) => {
   return (
     <input
+      onBlur={onBlur}
+      className={className}
       checked={checked}
       type={type}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
+      data-testid={testID}
     />
   );
 };
