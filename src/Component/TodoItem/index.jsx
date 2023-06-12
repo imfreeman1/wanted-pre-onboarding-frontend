1d5bf5e8f91f51033f4ff084c9497ce9a4352d70
imfreeman1 <dkfrhkt@gmail.com>
2023-06-12 19:21:54 +0900

    TodoItem component 변경
     - modifyContent state 생성
     - 통신 함수 생성

--- src/Component/TodoItem/index.jsx
+++ src/Component/TodoItem/index.jsx
@@ -1,66 +1,104 @@
 import React, { useContext, useState, useCallback } from "react";
-import Button from "../Button";
-import Input from "../Input";
 import { TodoDispatch } from "../../Context/todoContext";
 import { TODO_ACTION } from "../../Context/action";
+import axios from "../../Utils/axios";
+import Presenter from "./Presenter";
 
 const TodoItem = ({ todo }) => {
   const dispatch = useContext(TodoDispatch);
   const [isEditing, setIsEditing] = useState(false);
-  const handleEditing = useCallback(() => {
+  const [modifyContent, setModifyContent] = useState(todo.content);
+
+  const handleEditing = useCallback(
+    async (task) => {
+      try {
+        if (isEditing) {
+          await axios.request({
+            url: `/todos/${task.id}`,
+            method: "put",
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+              "Content-Type": "application/json",
+            },
+            data: {
+              todo: task.content,
+              isCompleted: task.isCompleted,
+            },
+          });
+          dispatch({
+            type: TODO_ACTION.MODIFY,
+            payload: { ...task, content: modifyContent },
+          });
+        }
+        setIsEditing(!isEditing);
+      } catch (error) {}
+    },
+    [isEditing, modifyContent, dispatch]
+  );
+  const handleModify = useCallback((e) => {
+    setModifyContent(e.target.value);
+  }, []);
+  const handleRemove = useCallback(
+    async (id) => {
+      try {
+        await axios.request({
+          url: `/todos/${todo.id}`,
+          method: "delete",
+          headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+        });
+        dispatch({ type: TODO_ACTION.REMOVE, payload: id });
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    [dispatch, todo.id]
+  );
+  const handleBlur = useCallback(() => {
     setIsEditing(!isEditing);
   }, [isEditing]);
-  const handleModify = useCallback(
-    (e, task) => {
-      dispatch({
-        type: TODO_ACTION.MODIFY,
-        payload: { ...task, content: e.target.value },
-      });
+  const handleCheck = useCallback(
+    async (task) => {
+      try {
+        await axios.request({
+          url: `/todos/${task.id}`,
+          method: "put",
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+            "Content-Type": "application/json",
+          },
+          data: {
+            todo: task.content,
+            isCompleted: !task.isCompleted,
+          },
+        });
+        dispatch({
+          type: TODO_ACTION.CHECK,
+          payload: { ...task, isCompleted: !task.isCompleted },
+        });
+      } catch (error) {
+        throw new Error(error);
+      }
     },
     [dispatch]
   );
-  const handleRemove = useCallback(
-    (id) => {
-      dispatch({ type: TODO_ACTION.REMOVE, payload: id });
-    },
-    [dispatch]
-  );
-  const handleCheck = (e, task) => {
-    dispatch({
-      type: TODO_ACTION.CHECK,
-      payload: { ...task, isCompleted: e.target.checked },
-    });
-  };
-  return (
-    <li key={todo.id}>
-      <div>
-        <label>
-          <Input
-            type="checkbox"
-            checked={todo.isCompleted}
-            onChange={(e) => handleCheck(e, todo)}
-          />
-          {!isEditing ? (
-            <>
-              <span>{todo.content}</span>
-              <Button children={"수정"} onClick={handleEditing} />
-            </>
-          ) : (
-            <>
-              <Input
-                type="text"
-                value={todo.content}
-                onChange={(e) => handleModify(e, todo)}
-              />
-              <Button children={"완료"} onClick={handleEditing} />
-            </>
-          )}
-        </label>
+  const handleCancel = useCallback(() => {
+    setModifyContent(todo.content);
+    setIsEditing(!isEditing);
+  }, [isEditing, todo.content]);
 
-        <Button children={"삭제"} onClick={() => handleRemove(todo.id)} />
-      </div>
-    </li>
+  return (
+    <Presenter
+      todo={todo}
+      isEditing={isEditing}
+      handleBlur={handleBlur}
+      handleCheck={handleCheck}
+      handleCancel={handleCancel}
+      handleRemove={handleRemove}
+      handleModify={handleModify}
+      handleEditing={handleEditing}
+      modifyContent={modifyContent}
+    />
   );
 };
 
-export default TodoItem;
+export default React.memo(TodoItem);
